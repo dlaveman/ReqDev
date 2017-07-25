f162902e7cef304fcaf9df7debcd8b5396f521c0
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-07-25 19:58:01 -0400

    Basic routing done

--- server/api.js
+++ server/api.js
@@ -6,6 +6,10 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
+  .use('/cart', require('./cart'))
+  .use('/order', require('./order'))
+  .use('/developer', require('./developer'))
+  .use('/categories', require('./category'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
---
+++ server/cart.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const db = require('APP/db')
+const Cart = db.model('carts')
+
+module.exports = require('express').Router()
+  .get('/:userId', (req, res, next) =>
+    Cart.findAll({ where: { user_id: req.params.userId } })
+      .then(carts => res.json(carts))
+      .catch(next))
+  .post('/', (req, res, next) =>
+    Cart.create(req.body)
+      .then(cart => res.status(201).json(cart))
+      .catch(next))
+  .put('/:cartId', (req, res, next) =>
+    Cart.update(req.body, {
+      where: { id: req.params.cartId },
+      returning: true
+    })
+      .then(cart => { res.json(cart) })
+      .catch(next))
+  .delete('/:userId', (req, res, next) => {
+    Cart.destroy({
+      where: { user_id: req.params.userId }
+    })
+      .catch(next)
+  })
+  .delete('/:userId/:cartId', (req, res, next) => {
+    Cart.destroy({
+      where: { id: req.params.cartId }
+    })
+      .catch(next)
+  })
