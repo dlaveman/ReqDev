7c766e5df1be727a2496fed873981a40c70a6120
deepseafishing <onejunekang@gmail.com>
2017-07-26 16:31:47 -0400

    finish setting for material-ui

--- app/main.jsx
+++ app/main.jsx
@@ -1,45 +1,52 @@
-'use strict'
+'use strict';
 
 /**
  * `babel-preset-env` converts this general import into a selection of specific
  * imports needed to polyfill the currently-supported environment (as specified
  * in `.babelrc`). As of 2017-06-04, this is primarily to support async/await.
  */
-import 'babel-polyfill'
+import 'babel-polyfill';
 
-import React from 'react'
-import {render} from 'react-dom'
-import {Provider, connect} from 'react-redux'
-import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+// material-ui uses react-tap-event-plugin
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
-import store from './store'
-import Jokes from './components/Jokes'
-import Login from './components/Login'
-import WhoAmI from './components/WhoAmI'
-import NotFound from './components/NotFound'
-import NavBar from './components/NavBar'
-import Categories from './components/Categories'
-import Home from './components/Home'
+import store from './store';
+import Jokes from './components/Jokes';
+import Login from './components/Login';
+import WhoAmI from './components/WhoAmI';
+import NotFound from './components/NotFound';
+import NavBar from './components/NavBar';
+import Categories from './components/Categories';
+import Home from './components/Home';
 
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-)(
-  ({ user, children }) =>
-    <div>
-      <NavBar/>
-      {/*<nav>
+const ExampleApp = connect(({ auth }) => ({
+  user: auth
+}))(({ user, children }) =>
+  <div>
+    <NavBar />
+    {/*<nav>
         {user ? <WhoAmI/> : <Login/>}
       </nav>*/}
-      <main>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/categories" component={Categories}/>
-          <Route path="/login" component={Login}/>
-          <Route component={NotFound} />
-        </Switch>
-      </main>
-    </div>
-)
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/categories" component={Categories} />
+        <Route path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </main>
+  </div>
+);
 
 render(
   <Provider store={store}>
@@ -48,4 +55,4 @@ render(
     </Router>
   </Provider>,
   document.getElementById('main')
-)
+);
