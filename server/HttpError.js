9e09ec18e3da73cbb221de18412f379f3bd22211
deepseafishing <onejunekang@gmail.com>
2017-07-27 11:12:08 -0400

    addes linter

--- server/HttpError.js
+++ server/HttpError.js
@@ -1,22 +1,22 @@
-'use strict';
+'use strict'
 // from Author Workshop to address what Kate said in the lecture.
 
-const http = require('http');
+const http = require('http')
 
 function HttpError(status, message) {
-	const err = new Error(message || http.STATUS_CODES[status]);
-	err.status = status;
-	Object.setPrototypeOf(err, HttpError.prototype);
-	return err;
+	const err = new Error(message || http.STATUS_CODES[status])
+	err.status = status
+	Object.setPrototypeOf(err, HttpError.prototype)
+	return err
 }
 
-Object.setPrototypeOf(HttpError.prototype, Error.prototype);
+Object.setPrototypeOf(HttpError.prototype, Error.prototype)
 
 HttpError.prototype.middleware = () => {
-	const self = this;
+	const self = this
 	return (req, res, next) => {
-		next(self);
-	};
-};
+		next(self)
+	}
+}
 
-module.exports = HttpError;
+module.exports = HttpError
