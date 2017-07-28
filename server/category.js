7573c3f51231dc84a0eefb21ac3124c33f78ca9f
Yu Lin <yu@Yus-MacBook-Pro.local>
2017-07-27 21:36:01 -0400

    fixed: category + developer routes

--- server/category.js
+++ server/category.js
@@ -2,17 +2,19 @@
 
 const db = require('APP/db')
 const Category = db.model('categories')
+const Developer = db.model('developers')
 
 module.exports = require('express').Router()
   .get('/', (req, res, next) =>
-    Category.findAll()
-      .then(categories => res.json(categories))
+    Category.findAll({
+      where: { name: req.query.category },
+      include: [
+        { model: Developer }
+      ]
+    })
+      .then(developer => res.json(developer))
       .catch(next))
   .post('/', (req, res, next) =>
     Category.create(req.body)
       .then(category => res.status(201).json(category))
-      .catch(next))
-  .get('/:categoryId', (req, res, next) =>
-    Category.findById(req.params.categoryId)
-      .then(category => res.json(category))
       .catch(next))
