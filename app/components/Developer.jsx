cf74ef1df983a1b4204f42a174b29617283ce04f
Stacy Hirschberg <stacyhi511@gmail.com>
2017-07-31 13:02:52 -0400

    in progress

--- app/components/Developer.jsx
+++ app/components/Developer.jsx
@@ -13,14 +13,21 @@ class Developer extends React.Component {
     this.props.fetchDeveloper(1) // hardcoding dev id
   }
 
+  handleAddToCart = (evt) => {
+    evt.preventDefault()
+    console.log('dev evt', this.props)
+  }
+
   render() {
     return (
       <div className="links">
-        <form>
+        <form onSubmit={this.handleAddToCart}>
           <h1>Hi, I'm {this.props.developers.name} </h1>
-          <h3>My rate is {this.props.developers.rate} </h3>
+          <h3>My rate is ${this.props.developers.rate} </h3>
           <h3>I'm reachable at {this.props.developers.email} </h3>
-          <input type="submit" value="Add To Cart" />
+          {
+            this.props.user && <input type="submit" value="Add To Cart" />
+          }
         </form>
       </div>
     )
@@ -30,6 +37,7 @@ class Developer extends React.Component {
 const mapStateToProps = state => {
   return {
     developers: state.developers,
+    user: state.auth
   }
 }
 const mapDispatchToProps = dispatch => {
