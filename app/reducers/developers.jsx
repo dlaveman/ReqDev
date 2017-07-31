6913aa45da59439ce8d51a1a825dec98027a3ca0
Stacy Hirschberg <stacyhi511@gmail.com>
2017-07-31 11:22:32 -0400

    fiexed conflicts

--- app/reducers/developers.jsx
+++ app/reducers/developers.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
 const GET_DEVELOPER = 'GET_DEVELOPER'
+const GET_DEVELOPERS = 'GET_DEVELOPERS'
 
+export const getDevelopers = developers => ({ type: GET_DEVELOPERS, developers })
 export const getDeveloper = developer => ({ type: GET_DEVELOPER, developer })
 
 export function fetchDeveloper(developerId) {
@@ -12,10 +13,9 @@ export function fetchDeveloper(developerId) {
       .then(developer => {
         console.log('Trying to get developer', developer)
         dispatch(getDeveloper(developer))
-=======
-const GET_DEVELOPERS = 'GET_DEVELOPERS'
-
-export const getDevelopers = developers => ({ type: GET_DEVELOPERS, developers })
+      })
+  }
+}
 
 export function fetchDevelopers(categoryName) {
   return function thunk(dispatch) {
@@ -24,22 +24,16 @@ export function fetchDevelopers(categoryName) {
       .then(developers => {
         console.log('Trying to get developers', developers)
         dispatch(getDevelopers(developers))
->>>>>>> 6fd594dda481ef3101185a56ec70d5cb1b561edd
       })
   }
 }
 
-<<<<<<< HEAD
 export default function developerReducer(state = [], action) {
   switch (action.type) {
   case GET_DEVELOPER:
     return action.developer
-=======
-export default function developersReducer(state = [], action) {
-  switch (action.type) {
   case GET_DEVELOPERS:
     return action.developers
->>>>>>> 6fd594dda481ef3101185a56ec70d5cb1b561edd
   default: return state
   }
 }
--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -5,11 +5,7 @@ import categories from './categories'
 import developers from './developers'
 
 const rootReducer = combineReducers({
-<<<<<<< HEAD
   auth, categories, developers
-=======
-	auth, categories, developers
->>>>>>> 6fd594dda481ef3101185a56ec70d5cb1b561edd
 })
 
 export * from './categories'
