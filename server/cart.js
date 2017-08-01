a202c675be636723f368718e271f603988acd01c
Stacy Hirschberg <stacyhi511@gmail.com>
2017-07-31 23:24:45 -0400

    fix: cart + - updates item only, no longer gets entire cart

--- server/cart.js
+++ server/cart.js
@@ -15,23 +15,33 @@ module.exports = require('express').Router()
     })
       .then(carts => res.json(carts))
       .catch(next))
+
   .post('/', (req, res, next) =>
     Cart.create(req.body)
       .then(cart => res.status(201).json(cart))
       .catch(next))
+
   .put('/:cartId', (req, res, next) =>
     Cart.update(req.body, {
       where: { id: req.params.cartId },
       returning: true
     })
+      .then(res => {
+        return Cart.findOne({
+          where: { id: res[1][0].id },
+          include: [{ model: Developer }],
+        })
+      })
       .then(cart => { res.json(cart) })
       .catch(next))
+
   .delete('/', (req, res, next) => {
     Cart.destroy({
       where: { user_id: req.query.user }
     })
       .catch(next)
   })
+
   .delete('/:cartId', (req, res, next) => {
     Cart.destroy({
       where: { id: req.params.cartId }
