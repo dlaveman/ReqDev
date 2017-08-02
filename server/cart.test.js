7d6210d96688301af6b4f56d8d9a401ad6739b1e
Stacy Hirschberg <stacyhi511@gmail.com>
2017-08-02 07:19:15 -0400

    cart api test

--- server/cart.test.js
+++ server/cart.test.js
@@ -1,42 +1,45 @@
+/* global beforeEach */
 const request = require('supertest')
   , { expect } = require('chai')
   , db = require('APP/db'), { Cart, User, Developer } = db
   , app = require('./start')
 
-var idCreated
 const alice = {
   username: 'alice@secrets.org',
   password: '12345',
   name: 'Alice',
 }
+
 /* global describe it before afterEach */
 
-describe('/api/cart/userId', () => {
+describe('/api/cart', () => {
+  let idCreated
+  const agent = request.agent(app)
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
-  beforeEach('create a cart', () => {
-    return Cart.create({
-      hours: 4,
-      user: {
-        email: 'alice@secrets.org',
-        name: 'Alice',
-      }
-    }, {
-      include: [ User ]
+  beforeEach('create a user and cart', () => {
+    return User.create({
+      email: alice.username,
+      password: alice.password,
+      name: alice.name,
+    })
+    .then((res) => {
+      idCreated = res.id
+      return agent.post('/api/auth/login/local').send(alice)
     })
       .then((res) => {
-        return idCreated = res.user_id
+        return Cart.create({
+          hours: 4,
+          user_id: idCreated
+        })
       })
+      .catch(err => console.log('err', err))
   })
-
-  describe('GET /cart', () => {
-    describe('when getting a cart/:userId', () =>
-      it('responds with the users cart', () =>
-        request(app).get('/api/cart/' + idCreated)
-          .expect(200)
-          .then(res => expect(res.body[0]).to.contain({
-            hours: 4
-          }))
-      ))
-  })
+  it('responds with the users cart', () =>
+    agent.get('/api/cart/')
+    .expect(200)
+    .then(res => expect(res.body[0]).to.contain({
+      hours: 4
+    }))
+  )
 })
